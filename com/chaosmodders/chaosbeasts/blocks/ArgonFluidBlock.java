2f547a1791059df98a2ac60f46dc399920d317ec
Eternaldoom <mezack@icloud.com>
2014-02-26 11:29:13 -0800

    tried to make argon less glitchy

--- com/chaosmodders/chaosbeasts/blocks/ArgonFluidBlock.java
+++ com/chaosmodders/chaosbeasts/blocks/ArgonFluidBlock.java
@@ -5,10 +5,29 @@ import net.minecraftforge.fluids.BlockFluidClassic;
 import com.chaosmodders.chaosbeasts.generic.ChaosHelper;
 import com.chaosmodders.chaosbeasts.generic.ChaosTabs;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class ArgonFluidBlock extends BlockFluidClassic {
 	public ArgonFluidBlock() {
 		super(ChaosBlocks.ArgonGasFluid, ChaosHelper.gas);
 		this.setCreativeTab(ChaosTabs.tabChaosBlocks);
 		this.setBlockTextureName("chaosbeasts:argon_gas");
 	}
+	
+	@Override
+    public boolean isOpaqueCube() {
+            return false;
+    }
+	
+	@Override
+	public boolean renderAsNormalBlock() {
+        return false;
+}
+	
+	@SideOnly(Side.CLIENT)
+    public int getRenderBlockPass()
+    {
+        return 1;
+    }
 }
