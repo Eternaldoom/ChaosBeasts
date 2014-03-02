17d63555a9676608b5048319485ffe6c1a7a6c04
Eternaldoom <mezack@icloud.com>
2014-03-01 21:34:17 -0800

    doom cape on fix hydrastone crash

--- com/chaosmodders/chaosbeasts/blocks/BlockHydrastone.java
+++ com/chaosmodders/chaosbeasts/blocks/BlockHydrastone.java
@@ -14,9 +14,6 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockHydrastone extends Block {
-	
-	public String particle;
-	public double upVel;
 
 	public BlockHydrastone()
 	{
@@ -27,26 +24,10 @@ public class BlockHydrastone extends Block {
 		this.setBlockName("hydratedStone");
 		this.setCreativeTab(ChaosTabs.tabChaosBlocks);
 		this.setLightLevel(1.0F);
-		
-		this.particle = "water_drip";
-		this.upVel = 0.5D;
 	}
 
 	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
     {
         return Item.getItemFromBlock(Blocks.cobblestone);
     }
-	
-	@SideOnly(Side.CLIENT)
-	  public void func_149734_b(World world, int x, int y, int z, Random rand)
-	  {
-	    if (!world.isBlockIndirectlyGettingPowered(x, y, z))
-	    {
-	      for (int i = 0; i < 20; i++)
-	      {
-	        world.spawnParticle(this.particle, x - 0.2D, y + (this.upVel + i / 10) / 5.0D, z + 0.5D, 0.0D, this.upVel + Math.random() * 10.0D, 0.0D);
-	        i = 0;
-	      }
-	    }
-	  }
 }
