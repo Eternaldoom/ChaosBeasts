c54115692b91ba4865c79da85d3106c5b86af719
Eternaldoom <mezack@icloud.com>
2014-03-27 09:31:01 -0700

    made it all better!

--- com/chaosmodders/chaosbeasts/blocks/BlockLaserBeam.java
+++ com/chaosmodders/chaosbeasts/blocks/BlockLaserBeam.java
@@ -9,7 +9,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 
-public class BlockLaserBeam extends Block{
+public class BlockLaserBeam extends Block {
 	public BlockLaserBeam()
 	{
 		super(Material.cactus);
@@ -20,6 +20,7 @@ public class BlockLaserBeam extends Block{
 		this.setCreativeTab(ChaosTabs.tabChaosBlocks);
 	}
 	
+	@Override
 	public void onBlockAdded(World p_149726_1_, int i, int j, int k)
     {
         if (!p_149726_1_.isRemote)
@@ -28,14 +29,16 @@ public class BlockLaserBeam extends Block{
         }
     }
 	
-	public void updateTick(World par1world, int p_149674_2_, int p_149674_3_, int p_149674_4_)
+	@Override
+	public void updateTick(World par1world, int x, int y, int z, Random par5random)
     {
-        if (par1world.getBlock(p_149674_2_ + 1, p_149674_3_, p_149674_4_) != this || par1world.getBlock(p_149674_2_ + 1, p_149674_3_, p_149674_4_) != ChaosBlocks.LaserSource)
+        if (par1world.getBlock(x - 1, y, z) != ChaosBlocks.LaserBeam && par1world.getBlock(x - 1, y, z) != ChaosBlocks.LaserSource)
         {
-            par1world.setBlock(p_149674_2_, p_149674_3_, p_149674_4_, Blocks.air);
+            par1world.setBlock(x, y, z, Blocks.air);
         }
     }
 	
+	@Override
     public void onNeighborBlockChange(World p_149695_1_, int p_149695_2_, int p_149695_3_, int p_149695_4_, Block p_149695_5_)
     {
         if (!p_149695_1_.isRemote)
